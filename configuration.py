db12232d95a56e2ab6ded9d19cfd714c92b6bb99
Danae <danaeblest@gmail.com>
2024-06-03 18:27:37 +0200

    mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-17bbc646-9086-4149-bad5-204951dfde8f.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
